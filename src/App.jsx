27ae49c9c3ee6395be07ca69f483b387354bb2ec
sofian benhaddou <sofianbenhaddou06100@gmail.com>
2023-01-04 11:44:04 +0100

    app3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Hero from "./component/product/product/Hero";
+import Productmain from "./component/product/Productmain";
 import Test from "./components/test";
 
 function App() {
-	return <Test />;
+	return (
+		<>
+		{/* <Test />, */}
+		<Productmain/>
+		<Hero/>,
+		</>
+	)
 }
 
 export default App;
