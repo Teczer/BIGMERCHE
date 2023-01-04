7d71fb7e2170a4ecdb9086dd6ad51cc9584ad12f
Mehdi <mehdi.hattou1@gmail.com>
2023-01-04 10:41:32 +0100

    fix merge from islame branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Api from './components/productsHome/Product'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <Api/>
-    </>
-  )
-}
-
-export default App
-=======
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Test from "./components/test";
@@ -24,4 +7,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 8a8f2cc98301edef414a1c4a893cb01a0803320c
